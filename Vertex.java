3e94f0bbf595cf26d838f5858fa46e4483413321
polygon707070 <polygon707070@tutamail.com>
2024-07-14 17:23:27 +0300

    update readme

---
+++ Vertex.java
@@ -0,0 +1,19 @@
+
+package GraphDDrawingProject;
+
+public class Vertex 
+{
+    private int index;
+    public Vertex (int index)
+    {
+        this.index = index;
+    }
+    void setIndex(int index)
+    {
+        this.index = index;
+    }
+    int getIndex()
+    {
+       return this.index;
+    }
+}
